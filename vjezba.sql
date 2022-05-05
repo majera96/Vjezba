d7ed8ed1d2a2128bcfc0e13791b48eabf63a6e49
majera96 <majer.antonio@gmail.com>
2022-05-05 14:21:39 +0200

    create vjezba.sql
    Napravljena tablica za ER dijagram sa entitetima i atributima koristenima za primjer

---
+++ vjezba.sql
@@ -0,0 +1,47 @@
+# Ovo je vjezba SQL sa sata ponovno pisana
+
+C:\xampp\mysql\bin\mysql -uroot <  C:\Users\Korisnik\Documents\GitHub\Vjezba
+
+drop database if exists;
+create database Vjezba;
+use Vjezba;
+
+create table Smjer (
+    Naziv varchar(50),
+    Cijena varchar(50),
+    Trajanje varchar(50),
+    Upisnina varchar(50),
+    Certificiran varchar (50)
+);
+
+create table Grupa (
+    Naziv varchar(50),
+    DatumPocetka varchar(50),
+    MaksimalnoPolaznika varchar(50),
+    Smjer varchar(50),
+    Predavac varchar(50),
+);
+
+create table Polaznik (
+    Osoba varchar(50),
+    BrojUgovora varchar(50)
+
+);
+
+create table Osoba (
+    Ime varchar(50),
+    Prezime varchar(50),
+    Email varchar(50),
+    Oib varchar(50)
+
+);
+
+create table Predavac (
+    Osoba varchar(50);
+    Iban (varchar 50)
+);
+
+create table Clan (
+    Grupa varchar(50),
+    Polaznik varchar (50)
+);
