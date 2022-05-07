004ab2fb9306d59dced34f9d18cdc260350dc601
majera96 <majer.antonio@gmail.com>
2022-05-07 17:47:17 +0200

    Create salon_za_uljepsavanje.sql
    Rjesavanje na temelju zadataka

---
+++ salon_za_uljepsavanje.sql
@@ -0,0 +1,32 @@
+#Salon za uljepsavanje
+
+drop database if exists;
+create database SalonZaUljepsavanje;
+use SalonZaUljepsavanje;
+
+create table Djelatnica(
+    Osoba varchar(50),
+    SifraDjelatnice varchar(50)
+);
+
+create table Osoba (
+    Ime varchar(50),
+    Prezime varchar(50),
+    Kontakt_broj varchar(50)
+);
+
+create table Korisnik(
+    Osoba varchar(50),
+    Usluga varchar(50)
+);
+
+create table Usluga(
+    Naziv varchar(50),
+    Vrijeme_usluge varchar(50),
+    Cijena varchar(50)
+);
+
+create table Narudzba(
+    Osoba varchar(50),
+    Usluga varchar(50)
+);
