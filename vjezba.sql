aaa66bf7328797e8d668e90ebd5980af07f3baee
majera96 <majer.antonio@gmail.com>
2022-05-10 17:07:26 +0200

    Update vjezba.sql
    zavrsena vjezba sa sata

--- vjezba.sql
+++ vjezba.sql
@@ -7,41 +7,62 @@ create database Vjezba;
 use Vjezba;
 
 create table Smjer (
-    Naziv varchar(50),
-    Cijena varchar(50),
-    Trajanje varchar(50),
-    Upisnina varchar(50),
-    Certificiran varchar (50)
+    sifra int not null primary key auto_increment,
+    Naziv varchar(50) not null,
+    Cijena decimal(18,2),
+    Trajanje int,
+    Upisnina decimal(18,2),
+    Certificiran boolean
 );
 
 create table Grupa (
-    Naziv varchar(50),
-    DatumPocetka varchar(50),
-    MaksimalnoPolaznika varchar(50),
-    Smjer varchar(50),
-    Predavac varchar(50)
+    sifra int not null primary key auto_increment,
+    Naziv varchar(50) not null,
+    DatumPocetka datetime,
+    MaksimalnoPolaznika int,
+    Smjer int not null,
+    Predavac int
 );
 
 create table Polaznik (
-    Osoba varchar(50),
-    BrojUgovora varchar(50)
+    sifra int not null primary key auto_increment,
+    Osoba int not null,
+    BrojUgovora varchar(10)
 
 );
 
 create table Osoba (
-    Ime varchar(50),
-    Prezime varchar(50),
-    Email varchar(50),
-    Oib varchar(50)
+    sifra int not null primary key auto_increment,
+    Ime varchar(50) not null,
+    Prezime varchar(50) not null,
+    Email varchar(100),
+    Oib char(11)
 
 );
 
 create table Predavac (
-    Osoba varchar(50),
+    sifra int not null primary key auto_increment,
+    Osoba int not null,
     Iban varchar(50)
 );
 
 create table Clan (
-    Grupa varchar(50),
-    Polaznik varchar (50)
+    sifra int not null primary key auto_increment,
+    Grupa int not null,
+    Polaznik int not null
 );
+
+alter table Grupa add foreign key (smjer) references smjer(sifra);
+alter table Grupa add foreign key (predavac) references predavac(sifra);
+
+alter table Clan add foreign key (grupa) references grupa(sifra);
+alter table Clan add foreign key (polaznik) references polaznik(sifra);
+
+alter table Polaznik add foreign key (osoba) references osoba(sifra);
+alter table Predavac add foreign key(osoba) references osoba(sifra);
+
+insert into Smjer (naziv)
+values ('PHP Programiranje');
+
+insert into Grupa (naziv, smjer)
+values ('PP25',1);
