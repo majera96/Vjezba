6d6725850ddb48cea1f09d256ea454d544d4fc45
majera96 <majer.antonio@gmail.com>
2022-05-12 10:57:19 +0200

    izrada era za postolar
    pokusaj izrade

--- postolar.sql
+++ postolar.sql
@@ -10,6 +10,7 @@ use Postolar;
 create table Postolar(
     sifra int not null primary key auto_increment,
     Osoba int not null,
+    Popravak int not null,
     Iskustvo boolean
 );
 
@@ -21,6 +22,7 @@ create table Klijent(
 );
 
 create table Osoba(
+    sifra int not null primary key auto_increment,
     Ime varchar(50) not null,
     Prezime varchar(50) not null,
     Kontakt int
@@ -28,7 +30,6 @@ create table Osoba(
 
 create table Obuca(
     sifra int not null primary key auto_increment,
-    Postolar int not null,
     Klijent int not null,
     Model varchar(50),
     Marka varchar(50),
@@ -50,3 +51,11 @@ create table Popravak(
     Cijena decimal(18,2),
     Kod varchar(5)
 );
+
+alter table Postolar add foreign key (Osoba) references Osoba(sifra);
+alter table Klijent add foreign key (Osoba) references Osoba(sifra);
+alter table Segrt add foreign key (Osoba) references Osoba(sifra);
+
+
+alter table Klijent add foreign key (Obuca) references Obuca(sifra);
+alter table Obuca add foreign key (Popravak) references Popravak(sifra);
