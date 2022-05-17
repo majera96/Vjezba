c69f91dcd355d4ea355bac1bb9e2a1cf510f6f0a
majera96 <majer.antonio@gmail.com>
2022-05-17 20:30:24 +0200

    zadaca
    rjesavanje zadace u dbeaveru

--- drzavna_samouprava.sql
+++ drzavna_samouprava.sql
@@ -37,3 +37,32 @@ alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
 
 
+#Zadaca
+
+insert into zupanija(sifra,naziv,zupan)
+values (null,'Osječko-baranjska županija',null),
+(null,'Istarska županija',null),
+(null,'Vukovarsko-srijemska županija',null);
+
+insert into opcina(sifra,zupanija,naziv)
+values (null,1,'Vuka'),(null,1,'Viškovci'),(null,2,'Marčana'),(null,2,'Fažana'),(null,3,'Nemetin'),(null,3,'Nuštar');
+
+insert into mjesto(sifra,opcina,naziv)
+values (null,1,'Beketinci'),(null,1,'Hrastovac'),(null,2,'Forkuševci'),(null,2,'Kuševac'),(null,3,'Buje'),(null,3,'Marčana'),(null,4,'Valbandon'),(null,4,'Fažana'),(null,5,'Negoslavci'),(null,5,'Bobota'),(null,6,'Nuštar'),(null,6,'Bogdanovci');
+
+insert into zupan(sifra,ime,prezime)
+values (null,'Ivan','Anušić'),(null,'Boris','Miletić'),(null,'Damir','Dekanić');
+
+update zupanija set zupan=1 where sifra=1;
+update zupanija set zupan=2 where sifra=2;
+update zupanija set zupan=3 where sifra=3;
+
+delete from opcina where sifra=4;
+delete from opcina where sifra=6;
+
+delete from mjesto where sifra=12;
+delete from mjesto where sifra=11;
+delete from mjesto where sifra=10;
+delete from mjesto where sifra=9;
+delete from mjesto where sifra=8;
+delete from mjesto where sifra=7;
