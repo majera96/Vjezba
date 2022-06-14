597bbaca5e9aedc8faebeeaa84b27b8003c45dc6
majera96 <majer.antonio@gmail.com>
2022-06-14 13:01:14 +0200

    Vjezba php sa sata
    Vjezba php

---
+++ www/phpuvod/03_hello.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    print 'Hello world';
+
+    ?>
+    <hr />
+
+    <?php
+
+    echo "Hello'World";
+    echo 'Hello"World';
+
+    echo 'Hello\'World';
+
+
+    echo 'Tekst<br />Novi red';
+    echo 'Prvi',PHP_EOL,'treći';
+    echo 'prvi'.'drugi'.'treći';
+
+
+
+
+
+
+
+    ?>
+
+
+</body>
+</html>
---
+++ www/phpuvod/zadatak01.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Kreirati stranicu koja ispisuje
+Vaše ime, prezime i adresu
+jedno ispod drugog.
+Ime, prezime i adresa se ispisuju iz PHP-a */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    echo 'Moje ime';
+    ?>
+    <br />
+    <?php
+    echo 'Moje prezime';
+    ?>
+    <br />
+    <?php
+    echo 'Moja adresa';
+    ?>
+
+
+
+
+</body>
+</html>
