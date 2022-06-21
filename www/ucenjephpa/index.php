1547a3538f67fb7b9f76444191dc2694e0055b4b
majera96 <majer.antonio@gmail.com>
2022-06-21 12:30:15 +0200

    implementacija
    implementacija sadrzaja

--- www/ucenjephpa/index.php
+++ www/ucenjephpa/index.php
@@ -1,16 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <title>Učenje PHP-a</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
 <body>
 
-<?php
-echo 'Pozdrav';
-?>
+    <div class="grid-container">
+
+    <!-- Start izbornik -->
+
+    <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+  <div class="title-bar-title">Učenje PHPa</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Učenje PHP-a</li>
+      <li>
+        <a href="#">One</a>
+        <ul class="menu vertical">
+          <li><a href="#">One</a></li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Two</a></li>
+      <li><a href="#">Three</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="Search"></li>
+      <li><button type="button" class="button">Search</button></li>
+    </ul>
+  </div>
+</div>
+
+   <!-- End izbornk -->
+
+   <!--Start tijelo -->
+
+   <div class="grid-x grid-margin-x" id="tijelo">
+        <div class="cell large-2 medium-4 small-6">
+          <div class="success callout">
+            Edunova
+          </div>
+        </div>
+        <div class="cell large-2 medium-4 small-6">
+          <div class="warning callout">  
+            Osijek
+          </div>
+        </div>
+        <div class="cell large-2 medium-4 small-6">
+          <div class="alert callout">
+            Ponedjeljak
+          </div>
+        </div>
+      </div>
+
+
+   <!--End tijelo -->
+
+   <!-- Start podnozje -->
+
+   <div class="grid-x">
+      <div class="cell">
+        <div class="callout">
+          &copy; Edunova 2022
+        </div>
+      </div>
+    </div>
+
+   <!-- End podnozje -->
+
+
     
-</body>
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
 </html>
