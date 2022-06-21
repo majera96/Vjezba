c5f36fa8b3bbf8c8191eab3de8a5152ac45f6699
majera96 <majer.antonio@gmail.com>
2022-06-21 12:35:25 +0200

    Create zadatak04.php

---
+++ www/phpuvod/zadatak04.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Kreirati stranicu koja prima tri GET parametra, ispisuje 
+jedno ispod drugog sva tri parametra
+zadatak03.php?p1=Edunova&p2=Osijek&p3=Nema interneta
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    <?php echo 'Edunova'; ?>
+    </p> <br />
+    
+    <?php echo 'Osijek'; ?>
+    </p> <br />
+
+    <?php echo 'Nema interneta'; ?>
+    </p>
+</body>
+</html>
