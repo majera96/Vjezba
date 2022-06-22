207594686c2aa53107a383245bc1cbef754c1676
majera96 <majer.antonio@gmail.com>
2022-06-22 11:41:04 +0200

    Update index.php
    dodatak izbornik

--- www/foundationzurb/index.php
+++ www/foundationzurb/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
+    <title>Učenje Foundation Zurba</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
   </head>
@@ -14,8 +14,23 @@
     <div class="grid-container">
  
       <!--Start izbornik -->
-
-
+<!-- Add a Sticky Menu -->
+<div data-sticky-container>
+  <div class="top-bar" data-sticky data-margin-top="0" id="example-menu">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">Učenje Foundation Zurba</li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu" data-magellan>
+        <li><a href="#first">One</a></li>
+        <li><a href="#second">Two</a></li>
+        <li><a href="#third">Three</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
       <!--end izbornik -->
 
       <!--Start tijelo -->
