1cc1793b47f8d0ca58028db983abe3b171495ee9
majera96 <majer.antonio@gmail.com>
2022-06-29 11:54:59 +0200

    implementacija foundation zurba

---
+++ www/zadatak2/grid.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php require_once 'zaglavlje.php'; ?>
+  </head>
+<body>
+
+    <div class="grid-container">
+
+    <?php include_once 'izbornik.php'; ?>
+
+    <!-- Start tijelo -->
+
+    <div class="row">
+    <div class="columns small-12 large-expand">One</div>
+    <div class="columns small-12 large-expand">Two</div>
+    <div class="columns small-12 large-expand">Three</div>
+    <div class="columns small-12 large-expand">Four</div>
+    <div class="columns small-12 large-expand">Five</div>
+    <div class="columns small-12 large-expand">Six</div>
+  </div>
+
+    <!-- End tijelo -->
+
+    <?php 
+    require_once 'podnozje.php'; ?>
