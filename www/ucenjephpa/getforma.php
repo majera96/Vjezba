b12298ae59aad71d74dfb10ae85c1cdc5ca2d7b6
majera96 <majer.antonio@gmail.com>
2022-07-11 12:00:18 +0200

    html forme

--- www/ucenjephpa/getforma.php
+++ www/ucenjephpa/getforma.php
@@ -1,9 +1,6 @@
-<?php
-
+<?php 
 $ime = isset($_GET['ime']) ? $_GET['ime'] : '';
-
 ?>
-
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
@@ -16,14 +13,77 @@ $ime = isset($_GET['ime']) ? $_GET['ime'] : '';
     <div class="grid-x grid-margin-x" id="tijelo">
       <div class="cell">
         <div class="callout">
-          Koristite gornji izbornik
-        </div>
-      </div>
-    </div>
-    <!-- End tijelo -->
-    <?php 
-        require_once 'podnozje.php'; ?>
-    </div>
-    <?php require_once 'jsskripte.php'; ?>
-  </body>
-</html>
+          
+
+          <form action="" method="get">
+            <!-- najbolja praksa -->
+            <label for="ime">Ime</label>
+            <input type="text" id="ime" name="ime"
+            value="<?=$ime?>" />
+
+            <!-- Foundation praksa -->
+            <label>Iznos
+              <input type="number" name="iznos"  />
+          </label>
+
+            <label for="boja">Boja</label>
+            <input type="color" id="boja" name="boja" />
+
+            <label for="datum">Datum</label>
+            <input type="date" id="datum" name="datum" />
+
+            <label for="vrijeme">Vrijeme</label>
+            <input type="datetime-local" id="vrijeme" name="vrijeme" />
+          
+           
+            <input 
+            placeholder="ivan.horvat@tvrtka.hr"
+            type="email" id="email" name="email" />
+        
+            <label for="lozinka">Lozinka</label>
+            <input
+            required 
+            type="password" id="lozinka" name="lozinka" />
+
+            <p>Jednostruki odabir</p>
+<input type="radio" name="gd" id="ljeto" value="1" checked="checked">
+<label for="ljeto">Ljeto</label> <br />
+
+<input type="radio" name="gd" id="zime" value="2" checked="checked">
+<label for="zima">Zima</label>
+
+            <p>Jednostruki odabir 2</p>
+<input type="radio" name="gd1" id="ljeto1" value="1">
+<label for="ljeto1">Ljeto</label> <br />
+
+<input type="radio" name="gd1" id="zima1" value="2">
+<label for="zima1">Zima</label>
+
+<p>Višestruki odabir</p>
+<input type="checkbox" name="vo[]" id="proljece" value="1">
+<label for="proljece">Proljece</label>
+
+<input type="checkbox" name="vo[]" id="jesen" value="2">
+<label for="jesen">Jesen</label>
+
+<p>Odabir s liste</p>
+<label for="stavka">Odaberi iz liste</label>
+<select name="stavka" id="stavka">
+<option value="1">Osijek</option>
+<option selected="selected" value="2">Đakovo</option>
+
+</select>
+
+<label for="opis">Opis</label>
+<textarea name="opis" id="opis" cols="30" rows="5"></textarea>
+
+<hr />
+
+<input type="submit" value="Predaj" > 
+
+<pre>
+            <?php 
+            var_dump($_GET);
+
+            ?>
+            </pre>
