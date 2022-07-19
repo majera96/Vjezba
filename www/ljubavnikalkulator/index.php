5923629a0adc374fae0b3b7dd6758e86e5cf433d
majera96 <majer.antonio@gmail.com>
2022-07-19 15:29:02 +0200

    Create index.php
    pocetak izrade ljubavnog kalkulatora

---
+++ www/ljubavnikalkulator/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h5>ljubavi Kalkulator</h5>
+<br />
+<h3>Unesite dva imena i izračunajte koliko se % volite</h3>
+
+<form action="" method="get">
+<label for="ime1"><p>Unesite prvo ime</p></label>
+<input type="text" name="ime1" id="ime1" value="ime1">
+<label for="ime2">Unesite drugo ime</label>
+<input type="text" id="ime2" name="ime2" value="ime2">
+<input class="success button expanded" type="submit" value="Izračunajte ljubav">
+<a class="alert button expanded" href="index.php">Resetiraj</a>
+</form>
+    
+</body>
+</html>
